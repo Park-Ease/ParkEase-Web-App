8f3429bcdc917a4876a2514301fb9ad3d289c27c
ShadowSlayer03 <sslayer441@gmail.com>
2024-11-19 00:34:56 +0530

    Made custom sign-in and sign-up pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,36 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div className="relative flex flex-col md:flex-row justify-center md:justify-end items-center h-screen w-screen">
+      {/* Background Banner */}
+      <div className="absolute inset-0 -z-10">
+        <Image
+          src="/parkease-banner.png"
+          layout="fill"
+          objectFit="cover"
+          alt="Parkease Logo"
+        />
+      </div>
+
+      {/* SignIn Section */}
+      <div className="w-full md:w-[40%] h-auto md:h-full  p-4 flex items-center justify-center">
+        <SignIn
+          appearance={{
+            elements:
+            {
+              headerTitle: 'text-2xl font-funnel-display font-bold',
+              headerSubtitle: 'text-md',
+              formFieldLabel: 'text-[1rem] mt-4',
+              formFieldInput: 'text-[1rem]',
+              formButtonPrimary: 'bg-black text-white text-[1rem]',
+              socialButtonsBlockButtonText: 'text-[0.9rem]',
+              footerActionText: 'text-[0.9rem]',
+              footerActionLink: 'text-[0.9rem]'
+            }
+          }} />
+      </div>
+    </div>
+  );
 }
--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,38 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from '@clerk/nextjs';
+import Image from 'next/image';
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="flex flex-col md:flex-row justify-center items-center h-screen w-screen bg-[#fcd904]/80">
+      {/* Banner Section */}
+      <div className="relative w-full md:w-[60%] h-[40%] md:h-full bg-black/90">
+        <Image
+          src="/parkease-adcard.png"
+          objectFit="cover"
+          objectPosition='center'
+          layout="fill"
+          alt="Parkease Logo"
+        />
+      </div>
+
+      {/* SignUp Form Section */}
+      <div className="w-full md:w-[40%] h-[60%] md:h-full flex items-center justify-center p-4">
+        <SignUp 
+        appearance={{
+          elements:
+          {
+            headerTitle: 'text-2xl font-funnel-display font-bold',
+            headerSubtitle: 'text-md',
+            formFieldLabel: 'text-[1rem]',
+            formFieldInput: 'text-[1rem]',
+            formButtonPrimary: 'bg-black text-white text-[1rem]',
+            socialButtonsBlockButtonText: 'text-[0.9rem]',
+            footerActionText: 'text-[0.9rem]',
+            footerActionLink: 'text-[0.9rem]'
+          }
+        }}
+         />
+      </div>
+    </div>
+  );
 }
