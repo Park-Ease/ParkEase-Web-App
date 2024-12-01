30daa3c2ae026e916199adbd6c74bc2bc6efc8d7
ShadowSlayer03 <sslayer441@gmail.com>
2024-12-02 01:34:37 +0530

    Modified some non-working code

--- src/app/auth/callback/page.jsx
+++ src/app/auth/callback/page.jsx
@@ -1,25 +1,14 @@
 import React from 'react';
 import checkUserAndCreate from '@/actions/checkUserAndCreate';
-import ToastHandler from '@/components/global/ToastHandler';
+import ToastHandler from '@/components/global/ToastHandler'; 
 
 const AuthCallback = async () => {
     const {status,message} = await checkUserAndCreate();
 
-    if(status === 200 || status === 201){
-        toast.success(message);
-        redirect("/");
-    }
-
-    else{
-        toast.error(message);
-        redirect("/sign-in");
-    }
-
-
   return (
     <div className="flex justify-center items-center">
         <h1 className="text-6xl font-semibold font-funnel-display">Authenticating...</h1>
-        <ToastHandler message={message} isError={status !== 200} />
+        <ToastHandler message={message} isError={status !== 200 || status!=201} redirectUrls={["/signin","/"]} />
     </div>
   )
 }
--- src/components/global/ToastHandler/index.jsx
+++ src/components/global/ToastHandler/index.jsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
-const ToastHandler = ({ message, isError }) => {
+// In the redirectUrls if provided, first one is failure and second one is success.
+const ToastHandler = ({ message, isError, redirectUrls = [] }) => {
+  const router = useRouter();
+
   if (message) {
     isError ? toast.error(message) : toast.success(message);
+
+    if (redirectUrls.length > 0) {
+      isError
+        ? router.push(redirectUrls[0])
+        : router.push(redirectUrls[1]);
+    }
   }
   return null;
 };
