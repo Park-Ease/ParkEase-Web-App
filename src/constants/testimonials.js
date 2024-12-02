c304930b3675cf671f689609774fe3d253d39649
ShadowSlayer03 <sslayer441@gmail.com>
2024-12-03 02:39:56 +0530

    Made changes to user designations in testimonial

--- src/constants/testimonials.js
+++ src/constants/testimonials.js
@@ -15,25 +15,25 @@ const testimonials = [
         stars: 5, 
         text: "Exceeded every expectation I had! The design is sleek, the functionality is top-notch, and it’s incredibly easy to integrate into our existing systems. This tool has transformed our operations and enhanced our team’s ability to collaborate effectively. I can't imagine going back to the old way of doing things.", 
         userName: "Ayush Singh", 
-        designation: "Designer" 
+        designation: "UI/UX Designer" 
     },
     { 
         stars: 3, 
         text: "The service is decent and fulfills most of the advertised features, but there are areas that could use some improvement. Specifically, I would love to see more customization options. Overall, it’s a good product, but with a few tweaks, it could be even better.", 
         userName: "Ashish Baghel", 
-        designation: "Developer" 
+        designation: "Software Developer" 
     },
     { 
         stars: 4, 
         text: "This product delivers solid performance across the board. It’s reliable and user-friendly, and has significantly reduced the amount of time I spend on mundane tasks. I’d recommend it for anyone looking for a straightforward solution to streamline their workflow.", 
         userName: "Sagnik Saha", 
-        designation: "Analyst" 
+        designation: "Data Analyst" 
     },
     { 
         stars: 5, 
         text: "I can't recommend this service enough! The team behind it is always innovating and updating features, keeping it fresh and relevant. It’s clear they genuinely care about their users and are dedicated to providing a top-notch experience.", 
         userName: "Ajay Biradar", 
-        designation: "Consultant" 
+        designation: "Technical Consultant" 
     },
 ];
 
