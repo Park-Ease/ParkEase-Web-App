a29131e995ab4cc2e85b140021d2b5a1bb917441
ShadowSlayer03 <sslayer441@gmail.com>
2025-01-19 16:24:22 +0530

    Added formatDate util function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(dateString) {
+  const date = new Date(dateString);
+
+  const day = date.getDate();
+  const daySuffix = day % 10 === 1 && day !== 11 ? "st" :
+                    day % 10 === 2 && day !== 12 ? "nd" :
+                    day % 10 === 3 && day !== 13 ? "rd" : "th";
+
+  const monthNames = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+  const month = monthNames[date.getMonth()];
+
+  const year = date.getFullYear();
+
+  return `${day}${daySuffix} ${month} ${year}`;
+}
+
